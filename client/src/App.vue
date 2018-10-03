3f6197a738ce7975552c21ab0c84fd36d57bb034
Ivan Company <pyivanc@gmail.com>
2018-10-02 22:55:20 -0700

    [day 1] Created first steps of the frontend

---
+++ client/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1>Hello, {{ name }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      name: 'Ivan'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+  #app {
+    color: #56b983;
+  }
+</style>
